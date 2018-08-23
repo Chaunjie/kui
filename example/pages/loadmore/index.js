5d8df6be32df3df84ee2e25927911a5c92a5e894
chaunjie <894576801@qq.com>
2018-08-23 12:26:39 +0800

    #0: add button, loading, loadmore components

---
+++ example/pages/loadmore/index.js
@@ -0,0 +1,19 @@
+
+import Page from '../../common/page';
+
+Page({
+  data: {
+  	more1: {
+      type: true,
+      loading: '加载中...',
+      loaded: '加载完成'
+    },
+    more2: {
+      type: false,
+      loading: '加载中',
+      loaded: '我们是有底线的'
+    }
+  },
+
+  onLoad() {}
+});

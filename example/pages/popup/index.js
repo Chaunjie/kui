bd3122cf62b6f6f29a4c6cbccbfe1c31f9349892
chaunjie <894576801@qq.com>
2018-08-24 10:09:10 +0800

    #0: add popup component

---
+++ example/pages/popup/index.js
@@ -0,0 +1,41 @@
+
+import Page from '../../common/page';
+import Popup from '../../dist/popup/index';
+
+Page({
+  data: {
+    popData: [
+      {
+        showPopup: false,
+        position: 'center',
+        maskHide: false
+      },
+      {
+        showPopup: false,
+        position: 'top',
+        maskHide: false
+      },
+      {
+        showPopup: false,
+        position: 'right'
+      },
+      {
+        showPopup: false,
+        position: 'bottom'
+      },
+      {
+        showPopup: false,
+        position: 'left'
+      }
+    ]
+  },
+  onClick (e) {
+    const index  = +e.currentTarget.dataset.index
+    Popup.show('#popup-'+index)
+  },
+  close (e) {
+    const index  = +e.currentTarget.dataset.index
+    Popup.hide('#popup-'+index)
+  },
+  onLoad() {}
+});
---
+++ packages/popup/index.js
@@ -0,0 +1,44 @@
+import Popup from './popup';
+
+Component({
+  externalClasses: ['custom-class'],
+  options: {
+    multipleSlots: true
+  },
+  properties: {
+    show: {
+      type: [Boolean, String],
+      value: false
+    },
+    position: {
+      type: String,
+      value: 'bottom'
+    },
+    maskHide: {
+      type: Boolean,
+      value: true
+    }
+  },
+
+  methods: {
+    show () {
+      this.setData({
+        show: true
+      })
+    },
+    hide () {
+      this.setData({
+        show: false
+      })
+    },
+    togglePopup () {
+      this.setData({
+        show: !this.data.show
+      })
+    }
+  }
+})
+
+const popup = new Popup()
+
+export default popup;

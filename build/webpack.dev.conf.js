868b931857949d4210fd437442180ce2220ed313
chaunjie <894576801@qq.com>
2018-09-04 10:16:00 +0800

    #0: add favicon

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -55,6 +55,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
+      favicon: './webdoc/favicon-9-4.ico',
       inject: true
     }),
     // copy custom static assets
